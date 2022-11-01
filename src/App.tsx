093303cfc2d79eb7663421c43f0b8df0bfe00128
MillerJ20 <miller.johnston17@gmail.com>
2022-10-31 20:16:12 -0700

    configs done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from './Components/Counter';
+
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<Counter />
+		</div>
+	);
+};
+
+export default App;
